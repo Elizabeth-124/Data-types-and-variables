d7b82d8d8ff78150ee16ee11f9ec499aabd823bf
Elizabeth-124 <elizabeth124.1234@gmail.com>
2025-11-07 19:55:48 +0000

    first commit

---
+++ Main.py
@@ -0,0 +1,14 @@
+name="penguin" #string
+age=9 #integer
+weight=9.5 #float
+is_student=True #boolean
+print(f"{name}\n{age}\n{float}\n{is_student}")
+print(f"datatype of {name}is{type(name)}")
+print(f"datatype of {age}is{type(age)}")
+print(f"datatype of {weight}is{type(weight)}")
+print(f"datatype of {is_student}is{type(is_student)}")
+age=float(age)
+weight=int(weight)
+print(f"datatype of {age}is{type(age)}")
+print(f"datatype of {weight}is{type(weight)}")
+
