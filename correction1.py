ba7710dcc3aae7e2f13930efd28f0adaba00548e
Elizabeth-124 <elizabeth124.1234@gmail.com>
2025-11-18 18:50:14 +0000

    Add files via upload

---
+++ correction1.py
@@ -0,0 +1,16 @@
+a=float(input("Enter a number"))
+b=float(input("Enter a another number"))
+sum=a+b
+difference=a-b
+product=a*b
+quotient=a/b
+print(f"The sum of {a} and {b} is {sum}")
+print(f"The differece between {a} and {b} is {difference}")
+print(f"The product of {a} and {b} is {product}")
+print(f"The quotient of {a} and {b} is {quotient}")
+print(f"{a} is greater than {b}:{a>b}")
+print(f"{a} is less than {b}:{a<b}")
+print(f"{a} is equal to {b}:{a==b}")
+print(f"{a} is not equal to {b}:{a!=b}")
+print(f"{a} is greater than or equal to {b}:{a>=b}")
+print(f"{a} is less than or equal to {b}:{a<=b}")
